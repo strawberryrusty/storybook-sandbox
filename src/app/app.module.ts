da242633279f31d84c53ddb7f7b7a4c92f808a8e
Prashanth Mohan <prashmohanuon@gmail.com>
2023-02-23 17:04:16 +0100

    container data added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+ import { TaskModule } from './components/task.module';
+ import { NgxsModule } from '@ngxs/store';
+ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+ import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+   TaskModule,
+    NgxsModule.forRoot([], {
+     developmentMode: !environment.production,
+   }),
+   NgxsReduxDevtoolsPluginModule.forRoot(),
+   NgxsLoggerPluginModule.forRoot({
+     disabled: environment.production,
+   }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
